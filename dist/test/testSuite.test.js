da6ed28c994b53b54ccabe70eb6b29f772aa75b2
Donald Wu <yeukfei02@gmail.com>
2021-12-12 15:15:29 +0800

    fix: 🐛 add dist js folder

---
+++ dist/test/testSuite.test.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const pocket_1 = require("./pocket");
+describe("main test", () => {
+    (0, pocket_1.pocketTest)();
+});
