2c7a5e53b69d8aabc837431b179abf3fe81e35e6
Donald Wu <yeukfei02@gmail.com>
2021-12-12 22:24:51 +0800

    fix: 🐛 fix js file

--- test/testSuite.test.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const pocket_1 = require("./pocket");
-describe("main test", () => {
-    (0, pocket_1.pocketTest)();
-});
